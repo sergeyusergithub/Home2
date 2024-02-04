d8bfb4e6a9fe3ce5dd5611c14e169526b80fbd73
Sergey Sergey <sergeymailnew@mail.ru>
2024-02-04 15:15:06 +0500

    Add project files.

---
+++ Home2/fraction.cpp
@@ -0,0 +1,84 @@
+﻿#include "fraction.hpp"
+
+
+void fraction::zero_excep(long long num){
+    if (num == 0){
+        throw std::exception("Error: Deveder by zero");
+    }
+}
+
+bool fraction::sign_fract(){
+    if ((numerator_ > 0 && denominator_ > 0) ||
+    (numerator_ < 0 && denominator_ < 0)){
+        return false;
+    }
+
+    return true;
+}
+
+
+void Sum(fraction& frac1,fraction& frac2){
+            
+    frac1.numerator_ = frac1.numerator_ * frac2.denominator_ + frac2.numerator_ * frac1.denominator_;
+    frac1.denominator_ *= frac2.denominator_;
+    frac1.sign_ = frac1.sign_fract();
+}
+
+void Multiply(fraction& frac1, fraction& frac2)
+{
+    frac1.numerator_ *= frac2.numerator_;
+    frac1.denominator_ *= frac2.denominator_;
+    frac1.sign_ ^= frac2.sign_;
+}
+
+void Sub(fraction& frac1, fraction& frac2)
+{
+    frac1.numerator_ = frac1.numerator_ * frac2.denominator_ - frac2.numerator_ * frac1.denominator_;
+    frac1.denominator_ *= frac2.denominator_;
+    frac1.sign_ = frac1.sign_fract();
+}
+
+void Division(fraction& frac1, fraction& frac2)
+{
+    frac1.numerator_ *= frac2.denominator_;
+    frac1.denominator_ *= frac2.numerator_;
+    frac1.sign_ ^= frac2.sign_;
+}
+
+
+
+
+
+void Sum(fraction& frac, long long num)
+{
+    fraction tmp(num, 1);
+    Sum(frac, tmp);
+}
+
+void Multiply(fraction& frac, long long num)
+{
+    fraction tmp(num, 1);
+    Multiply(frac, tmp);
+}
+
+void Sub(fraction& frac, long long num)
+{
+    fraction tmp(num, 1);
+    Sub(frac, tmp);
+}
+
+void Division(fraction& frac, long long num)
+{
+    fraction tmp(num, 1);
+    Division(frac, tmp);
+}
+
+float fraction::Real_Number(){
+   return numerator_*(1.0)/denominator_;
+}
+
+bool fraction::Proper_fraction() {
+    return abs(numerator_) >= abs(denominator_) ? false : true;
+}
+
+
---
+++ Home2/fraction.hpp
@@ -0,0 +1,76 @@
+﻿#pragma once
+
+#ifndef FRACTION_HPP
+#define FRACTION_HPP
+
+#include <iostream>
+
+// класс дробь
+// раз нет публичных методов get и set, то
+// большинство методов для вычисления используется в виде дружественных функций
+
+
+class fraction {
+public:
+
+// конструктор по умолчанию
+fraction():numerator_(0),denominator_(1),sign_(false){}
+
+// конструктор с двумя параметрами
+// числитель и знаменатель 
+fraction(long long num,long long deno):numerator_(num),
+            denominator_(deno){
+    zero_excep(deno);
+    this->sign_ = sign_fract();
+}
+
+// метод суммы двух дробей
+friend void Sum(fraction& frac1,fraction& frac2);
+
+// метод произведения двух дробей
+friend void Multiply(fraction& frac1,fraction& frac2);
+
+// метод разности двух дробей
+friend void Sub(fraction& frac1, fraction& frac2);
+
+// метод деления двух дробей
+friend void Division(fraction& frac1, fraction& frac2);
+
+
+// метод сложения дроби и целого числа
+friend void Sum(fraction& frac, long long num);
+
+// метод произведения дроби и целого числа
+friend void Multiply(fraction& frac, long long num);
+
+// метод разности дроби и целого числа
+friend void Sub(fraction& frac, long long num);
+
+// метод деления дроби и целого числа
+friend void Division(fraction& frac, long long num);
+
+
+// метод возвращающий действительное число дроби
+float Real_Number(); 
+
+// метод определяющий состояние правильности дроби
+bool Proper_fraction(); // false - дробь не правильная
+                        // true - дробь правильная
+
+private:
+long long numerator_; // числитель
+long long denominator_; // знаменатель
+bool sign_; //знак дроби false - положительный
+            // true - отрицаетльный
+// метод проверки знаменателя на 0 и бросающая исключение
+void zero_excep(long long num);
+
+// метод определяющий знак дроби
+bool sign_fract();
+};
+
+
+
+
+
+#endif //FRACTION_HPP
